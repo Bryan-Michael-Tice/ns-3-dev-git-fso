9eb44b2551ee20393cad5ca9442f2413d605b3cd
Michael Di Perna <diperna.michael@gmail.com>
2016-06-28 11:38:07 -0400

    Added abstract base class FsoPhy (templated from SpectrumPhy). Updated laser-model header file to match source.

---
+++ src/fso/model/fso-phy.cc
@@ -0,0 +1,57 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2009 CTTC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Nicola Baldo <nbaldo@cttc.es>
+ *
+ * Modified by: Michael Di Perna <diperna.michael@gmail.com> 2016
+ */
+
+#include "fso-phy.h"
+#include <ns3/net-device.h>
+#include <ns3/mobility-model.h>
+#include <ns3/fso-channel.h>
+#include <ns3/log.h>
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("FsoPhy");
+
+NS_OBJECT_ENSURE_REGISTERED (FsoPhy);
+
+
+TypeId
+FsoPhy::GetTypeId (void)
+{
+  static TypeId tid = TypeId ("ns3::FsoPhy")
+    .SetParent<Object> ()
+    .SetGroupName ("Fso")
+  ;
+  return tid;
+}
+
+FsoPhy::FsoPhy ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+FsoPhy::~FsoPhy ()
+{
+  NS_LOG_FUNCTION (this);
+}
+
+
+} // namespace
---
+++ src/fso/model/fso-phy.h
@@ -0,0 +1,122 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2009 CTTC
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Nicola Baldo <nbaldo@cttc.es>
+ *
+ * Modified by: Michael Di Perna <diperna.michael@gmail.com> 2016
+ */
+
+#ifndef FSO_PHY_H
+#define FSO_PHY_H
+
+
+#include <ns3/object.h>
+#include <ns3/nstime.h>
+
+namespace ns3 {
+
+//class PacketBurst;
+class FsoChannel;
+class MobilityModel;
+class LaserModel;
+class NetDevice;
+
+/**
+ * \ingroup fso
+ *
+ * Abstract base class for free-space optical PHY layers
+ *
+ */
+class FsoPhy  : public Object
+{
+
+public:
+  FsoPhy ();
+  virtual ~FsoPhy ();
+
+  /**
+   * \brief Get the type ID.
+   * \return the object TypeId
+   */
+  static TypeId GetTypeId (void);
+
+  /**
+   * Set the associated NetDevice instance
+   *
+   * @param d the NetDevice instance
+   */
+  virtual void SetDevice (Ptr<NetDevice> d) = 0;
+
+  /**
+   * Get the associated NetDevice instance
+   *
+   * @return a Ptr to the associated NetDevice instance
+   */
+  virtual Ptr<NetDevice> GetDevice () const = 0;
+
+  /**
+   * Set the mobility model associated with this device.
+   *
+   * @param m the mobility model
+   */
+  virtual void SetMobility (Ptr<MobilityModel> m) = 0;
+
+  /**
+   * Get the associated MobilityModel instance
+   *
+   * @return a Ptr to the associated MobilityModel instance
+   */
+  virtual Ptr<MobilityModel> GetMobility () = 0;
+
+  /**
+   * Set the channel attached to this device.
+   *
+   * @param c the channel
+   */
+  virtual void SetChannel (Ptr<FsoChannel> c) = 0;
+
+private:
+  /**
+   * \brief Copy constructor
+   *
+   * Defined and unimplemented to avoid misuse
+   */
+  FsoPhy (FsoPhy const &);
+  /**
+   * \brief Copy constructor
+   *
+   * Defined and unimplemented to avoid misuse
+   * \returns
+   */
+  FsoPhy& operator= (FsoPhy const &);
+};
+
+
+
+
+
+
+
+
+
+} // namespace ns3
+
+
+
+
+
+#endif /* FSO_PHY_H */
