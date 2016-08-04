5507dd9d20d4ffb9f1dba076f370cf11b4a7d73c
Michael Di Perna <diperna.michael@gmail.com>
2016-08-04 09:37:37 -0400

    Patch applied to test suite.

--- src/fso/test/fso-propagation-loss-model-test-suite.cc
+++ src/fso/test/fso-propagation-loss-model-test-suite.cc
@@ -24,23 +24,27 @@
 #include "ns3/config.h"
 #include "ns3/double.h"
 #include "ns3/propagation-loss-model.h"
+#include "ns3/fso-mean-irradiance-model.h"
+#include "ns3/fso-down-link-scintillation-index-model.h"
+#include "ns3/fso-free-space-loss-model.h"
 #include "ns3/constant-position-mobility-model.h"
 #include "ns3/simulator.h"
+#include <cmath>
 
 using namespace ns3;
 
-NS_LOG_COMPONENT_DEFINE ("FsoPropagationLossModelsTest");
+NS_LOG_COMPONENT_DEFINE ("FsoPropagationLossTest");
 
 // ===========================================================================
 // This is a simple test to validate propagation loss models of ns-3 fso.
 // See the chapter in the ns-3 testing and validation guide for more detail
 // ===========================================================================
 //
-class FsoMeanIrradianceModelTestCase : public TestCase
+class FsoMeanIrradianceTestCase : public TestCase
 {
 public:
-  FsoMeanIrradianceModelTestCase ();
-  virtual ~FsoMeanIrradianceModelTestCase ();
+  FsoMeanIrradianceTestCase ();
+  virtual ~FsoMeanIrradianceTestCase ();
 
 private:
   virtual void DoRun (void);
@@ -58,19 +62,19 @@ private:
   TestVectors<TestVector> m_testVectors;
 };
 
-FsoMeanIrradianceModelTestCase::FsoMeanIrradianceModelTestCase ()
-  : TestCase ("Check to see that the ns-3 mean irradiance model provides the correct power density"), m_testVectors ()
+FsoMeanIrradianceTestCase::FsoMeanIrradianceTestCase ()
+  : TestCase ("Check to see that the ns-3 mean irradiance model provides the correct power density"),
+    m_testVectors ()
 {
 }
 
-FsoMeanIrradianceModelTestCase::~FsoMeanIrradianceModelTestCase ()
+FsoMeanIrradianceTestCase::~FsoMeanIrradianceTestCase ()
 {
 }
 
 void
-FsoMeanIrradianceModelTestCase::DoRun (void)
+FsoMeanIrradianceTestCase::DoRun (void)
 {
- 
   // We want to test the fso propagation loss model calculations for a pre-defined scenario 
   // and compare the results to those we have manually calculated
   // according to the model documentation, within the specificed tolerance
@@ -85,34 +89,35 @@ FsoMeanIrradianceModelTestCase::DoRun (void)
   testVector.m_txPhaseFrontRadius = 707000;
   testVector.m_meanIrradiance = 3.56696705922e-4;
   testVector.m_tolerance = 1e-9;  
+
   m_testVectors.Add (testVector);
 
   // Now, check that the received power values are expected
 
-  Ptr<MobilityModel> a = CreateObject<ConstantPositionMobilityModel> (); 
+  Ptr<MobilityModel> a = CreateObject<ConstantPositionMobilityModel> ();
   a->SetPosition (Vector (0,0,0));
-  Ptr<MobilityModel> b = CreateObject<ConstantPositionMobilityModel> (); 
+  Ptr<MobilityModel> b = CreateObject<ConstantPositionMobilityModel> ();
 
-  Ptr<FriisPropagationLossModel> lossModel = CreateObject<FriisPropagationLossModel> (); 
+  Ptr<FsoMeanIrradianceModel> lossModel = CreateObject<FsoMeanIrradianceModel> ();
   for (uint32_t i = 0; i < m_testVectors.GetN (); ++i)
     {
       testVector = m_testVectors.Get (i);
       b->SetPosition (Vector (0,0, testVector.m_distance));
-        
+
 
       double rxDiffractiveBeamRadius = lossModel->CalculateDiffractiveBeamRadius (testVector.m_distance, testVector.m_frequency, testVector.m_txBeamRadius, testVector.m_txPhaseFrontRadius);
-      double resultMeanIrradiance = lossModel->CalculateMeanIrradiance (testVector.m_txBeamradius, rxDiffractiveBeamRadius);
+      double resultMeanIrradiance = lossModel->CalculateMeanIrradiance (testVector.m_txBeamRadius, rxDiffractiveBeamRadius);
 
       NS_TEST_EXPECT_MSG_EQ_TOL (resultMeanIrradiance, testVector.m_meanIrradiance, testVector.m_tolerance, "Got unexpected mean irradiance at the receiver");
     }
 }
 
 
-class FsoDownLinkScintillationIndexModelTestCase : public TestCase
+class FsoDownLinkScintillationIndexTestCase : public TestCase
 {
 public:
-  FsoDownLinkScintillationIndexModel ();
-  virtual ~FsoDownLinkScintillationIndexModel ();
+  FsoDownLinkScintillationIndexTestCase ();
+  virtual ~FsoDownLinkScintillationIndexTestCase ();
 
 private:
   virtual void DoRun (void);
@@ -130,18 +135,18 @@ private:
   TestVectors<TestVector> m_testVectors;
 };
 
-FsoDownLinkScintillationIndexModel::FsoDownLinkScintillationIndexModel ()
-  : TestCase ("Check to see that the ns-3 TwoRayGround propagation loss model provides correct received power"),
+FsoDownLinkScintillationIndexTestCase::FsoDownLinkScintillationIndexTestCase ()
+  : TestCase ("Check to see that the ns-3 scintillation index model provides the scintilliation index"),
     m_testVectors ()
 {
 }
 
-FsoDownLinkScintillationIndexModel::~FsoDownLinkScintillationIndexModel ()
+FsoDownLinkScintillationIndexTestCase::~FsoDownLinkScintillationIndexTestCase ()
 {
 }
 
 void
-FsoDownLinkScintillationIndexModel::DoRun (void)
+FsoDownLinkScintillationIndexTestCase::DoRun (void)
 {
 
   // We want to test the fso propagation loss model calculations for a pre-defined scenario 
@@ -155,16 +160,16 @@ FsoDownLinkScintillationIndexModel::DoRun (void)
   testVector.m_txHeight = 707000;
   testVector.m_rxHeight = 0;
   testVector.m_frequency = frequency;
-  testVector.m_zenith = 60.0*M_PI/180.0;
+  testVector.m_zenith = (60.0 * M_PI)/180.0;
   testVector.m_tolerance = 1e-6;
   testVector.m_scintIndex = 0.16541409546;
-  m_testVectors.Add (testVector);
 
+  m_testVectors.Add (testVector);
 
   Ptr<FsoDownLinkScintillationIndexModel> lossModel = CreateObject<FsoDownLinkScintillationIndexModel> (); 
 
-  lossModel->SetRmsWindSpeed(21.0);
-  lossModel->SetGndRefractiveIdx(1.7e-14);
+  lossModel->SetRmsWindSpeed (21.0);
+  lossModel->SetGndRefractiveIdx (1.7e-14);
 
   for (uint32_t i = 0; i < m_testVectors.GetN (); ++i)
     {
@@ -175,11 +180,12 @@ FsoDownLinkScintillationIndexModel::DoRun (void)
     }
 }
 
-class FsoFreeSpaceLossModelTestCase : public TestCase
+class FsoFreeSpaceLossTestCase : public TestCase
 {
 public:
-  FsoFreeSpaceLossModelTestCase ();
-  virtual ~FsoFreeSpaceLossModelTestCase ();
+
+  FsoFreeSpaceLossTestCase ();
+  virtual ~FsoFreeSpaceLossTestCase ();
 
 private:
   virtual void DoRun (void);
@@ -195,18 +201,18 @@ private:
   TestVectors<TestVector> m_testVectors;
 };
 
-FsoFreeSpaceLossModelTestCase::FsoFreeSpaceLossModelTestCase ()
-  : TestCase ("Check to see that the ns-3 TwoRayGround propagation loss model provides correct received power"),
+FsoFreeSpaceLossTestCase::FsoFreeSpaceLossTestCase ()
+  : TestCase ("Check to see that the ns-3 free space loss model provides the correct loss"),
     m_testVectors ()
 {
 }
 
-FsoFreeSpaceLossModelTestCase::~FsoFreeSpaceLossModelTestCase ()
+FsoFreeSpaceLossTestCase::~FsoFreeSpaceLossTestCase ()
 {
 }
 
 void
-FsoFreeSpaceLossModelTestCase::DoRun (void)
+FsoFreeSpaceLossTestCase::DoRun (void)
 {
 
   // We want to test the fso propagation loss model calculations for a pre-defined scenario 
@@ -219,9 +225,8 @@ FsoFreeSpaceLossModelTestCase::DoRun (void)
   testVector.m_wavelength = 847e-9;
   testVector.m_tolerance = 1e-6;
   testVector.m_loss = 260.41;
+
   m_testVectors.Add (testVector);
-
-
 
   Ptr<FsoFreeSpaceLossModel> lossModel = CreateObject<FsoFreeSpaceLossModel> (); 
 
@@ -235,17 +240,18 @@ FsoFreeSpaceLossModelTestCase::DoRun (void)
 }
 
 
-class FsoPropagationLossModelsTestSuite : public TestSuite
+class FsoPropagationLossTestSuite : public TestSuite
 {
 public:
-  FsoPropagationLossModelsTestSuite ();
+  FsoPropagationLossTestSuite ();
 };
 
-FsoPropagationLossModelsTestSuite::PropagationLossModelsTestSuite ()
+FsoPropagationLossTestSuite::FsoPropagationLossTestSuite ()
   : TestSuite ("propagation-loss-model", UNIT)
 {
-  AddTestCase (new FsoMeanIrradianceModelTestCase, TestCase::QUICK);
-  AddTestCase (new FsoDownLinkScintillationIndexModel, TestCase::QUICK);
+  AddTestCase (new FsoMeanIrradianceTestCase, TestCase::QUICK);
+  AddTestCase (new FsoDownLinkScintillationIndexTestCase, TestCase::QUICK);
+  AddTestCase (new FsoFreeSpaceLossTestCase, TestCase::QUICK);
 }
 
-static FsoPropagationLossModelsTestSuite fsoPropagationLossModelsTestSuite;
+static FsoPropagationLossTestSuite fsoPropagationLossTestSuite;
